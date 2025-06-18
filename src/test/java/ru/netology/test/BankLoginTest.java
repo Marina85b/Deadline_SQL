7da4d5529c6c383016d05e632b6e4e18472b1504
Neto Marina <koshkalisa2018@yandex.ru>
2025-06-18 11:25:54 +0300

    add README

--- src/test/java/ru/netology/test/BankLoginTest.java
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -30,7 +30,7 @@ class BankLoginTest {
     // метод перед каждым автотестом открытием страницы помещаем ее в переменную LoginPage
     @BeforeEach
     void setUp() {
-        loginPage = open("http://localhost:9999", LoginPage.class);
+        loginPage = open("http://localhost:7777", LoginPage.class);
     }
 
     @Test
@@ -74,3 +74,5 @@ class BankLoginTest {
         verificationPage.verifyErrorNotification("Превышено максимальное количество попыток авторизации");
     }
 }
+
+
